83f5da0622a99de1613e2a3e64b86335426bd6d3
Nicholas <nicholas.lacey.2022@student.lhschools.org>
2019-05-22 14:17:32 -0400

    im sorry, but thank you

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,7 @@
 package com.LickingHeights;
-
 public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] Usless) {
+	System.out.print("Lost all my progress to a hard delete,\n time to see how fast i can work!");
+	System.out.println("though i dod not complete your project just know you were a great teacher\n and i hope you have a great summer!");
     }
 }
